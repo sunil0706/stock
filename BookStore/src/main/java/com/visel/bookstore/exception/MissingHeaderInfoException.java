9e54c67eaa8a00fcaba540912c02c9e9bdb15a7a
sunilnayak <sunilnayak@localhost>
2020-07-01 11:20:01 +0530

    Exception fill commit

---
+++ BookStore/src/main/java/com/visel/bookstore/exception/MissingHeaderInfoException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.visel.bookstore.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author sunilnayak
+ *
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class MissingHeaderInfoException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public MissingHeaderInfoException(String message) {
+        super(message);
+    }
+}
