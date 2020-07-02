bee55e801b57bfabb854212fa5586279742b3b65
sunilnayak <sunilnayak@localhost>
2020-07-02 12:05:19 +0530

    Build changes

--- BookStore/src/main/java/com/visel/bookstore/controller/FileUploadController.java
+++ BookStore/src/main/java/com/visel/bookstore/controller/FileUploadController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,17 +25,19 @@ import com.visel.bookstore.service.BookStoreServiceImpl;
  */
 @RestController
 public class FileUploadController {
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private BookStoreServiceImpl bookService;
 	
 	private static Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 	@RequestMapping(value="/fileUpload",method=RequestMethod.POST,consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
-    public Bookstore convertXMLtoJson()throws IOException {
+    public Bookstore convertXMLtoJava()throws IOException {
 		logger.info("Inside FileUploadController...");
-		
+		String path = env.getProperty("fileUploadpath");
 		XmlMapper mapper = new XmlMapper();
-		Bookstore bookstore = mapper.readValue(new File("/Users/sunilnayak/git/repository/BookStore/src/main/resources/bookstore.xml"), Bookstore.class);
+		Bookstore bookstore = mapper.readValue(new File(path), Bookstore.class);
 		logger.info("Outside FileUploadController...");
 		bookService.saveXMLObject(bookstore);
 		return bookstore;
